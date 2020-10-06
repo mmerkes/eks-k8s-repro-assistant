660dae551e8a3cf6ac18b056d38ddeff8b300626
Matt Merkes <merkes@amazon.com>
2020-10-06 15:13:36 -0700

    Updated disk IO generator to create many small files

--- utilities/disk-io-generator/disk-io-generator.sh
+++ utilities/disk-io-generator/disk-io-generator.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 while :
 do
+    LOG_FILE=log.txt
     FOO_FILE=foo
-    BAR_FILE=bar
     touch $FOO_FILE
-    for i in {1..5000}
+    echo "Starting to write foo file" >> $LOG_FILE
+    # 1K
+    head -c 1024 < /dev/urandom > $FOO_FILE
+
+    echo "Starting to copy file" >> $LOG_FILE
+    # Write 500MB
+    for i in {1..62500}
     do
-       echo "Uneasy lies the head that wears the crown." >> $FOO_FILE
+        cp $FOO_FILE $FOO_FILE$i 2>> $LOG_FILE
     done
 
-    for i in {1..5000}
-    do
-        cp $FOO_FILE $BAR_FILE
-        rm $BAR_FILE
-    done
-
-    rm $FOO_FILE
+    rm $FOO_FILE* 2>> $LOG_FILE
 done
